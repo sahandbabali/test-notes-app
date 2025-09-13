33482fec51d0771024b98184ca4435fc2107761e
Sahand <sahand.babali@gmail.com>
2025-09-13 21:44:57 +0330

    header added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "../contexts/AuthContext";
 
+// Simple Header Component
+function Header() {
+  return (
+    <header className="app-header">
+      <div className="header-container">
+        <h1 className="app-title">📝 Notes App</h1>
+      </div>
+    </header>
+  );
+}
+
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Header />
       <Component {...pageProps} />
     </AuthProvider>
   );
